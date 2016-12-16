c7edbc746edb117c2a725ce6d591c494b3c464d9
Hill <hill@kimo.com>
2016-12-16 09:45:01 +0000

    Add develop mode overwrite

--- getenv.php
+++ getenv.php
@@ -7,23 +7,32 @@ ${_INIT_CONFIG}[_CLASS] = __NAMESPACE__.'\getenv';
 
 \PMVC\initPlugin(['get'=>null]);
 
+/**
+ * @parameters bool isDev
+ */
 class getenv extends \PMVC\PlugIn
     implements GetInterface
 {
     public function get($k)
     {
-        if (isset($this[$k])) {
+        if ($this['isDev'] && \PMVC\value($_REQUEST,['--'.$k])) {
+
+            return $_REQUEST['--'.$k];
+        } elseif (isset($this[$k])) {
             if (is_callable($this[$k])) {
                 $isCache = false;
                 $v = $this[$k]($isCache, $k, $this);
                 if ($isCache) {
                     $this[$k] = $v;
                 }
+
                 return $v;
             } else {
+
                 return $this[$k];
             }
         } else {
+
             return \PMVC\value($_SERVER,[$k]);
         }
     }
